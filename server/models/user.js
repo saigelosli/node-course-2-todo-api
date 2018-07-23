0a327acea4ecb1c1da1d72006cb1ded86aa39462
Saige Losli <saige.losli@gmail.com>
2018-07-23 14:59:48 -0700

    Corrected last test

--- server/models/user.js
+++ server/models/user.js
@@ -49,10 +49,8 @@ UserSchema.methods.generateAuthToken = function() {
   var access = 'auth';
   var token = jwt.sign( { _id: user._id.toHexString(), access }, 'abc123' ).toString();
 
-  console.log( 'Tokens before: ', user.tokens );
   //user.tokens.push({ access, token });
   user.tokens = user.tokens.concat([{access, token}]);
-  console.log( 'Tokens after : ', user.tokens );
 
   return user.save().then( () => {
     return token;
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -251,7 +251,10 @@ describe( 'POST /users', () => {
 
     request(app)
       .post( '/users' )
-      .send( { email, password } )
+      .send( {
+        email: users[0].email,
+        password: users[0].password
+      } )
       .expect( 400 )
       .end( done );
   });
